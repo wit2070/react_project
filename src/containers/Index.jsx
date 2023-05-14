8827fd69dee70b4f7879d2ef3625523cf71b11bd
wit2070 <hsy3225@gmail.com>
2023-05-14 17:04:13 +0900

    git commit

--- src/containers/Index.jsx
+++ src/containers/Index.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react';
-import BtnSample from '../components/BtnSample'
+import BtnSample from '../components/BtnSample';
 
 export default function Index() {
-  const [counter,setCount] = useState(0);
-  function increase(){
+  const [counter, setCount] = useState(0);
+  function increase() {
     setCount(counter + 1);
-}
+  }
   return (
     <>
-    <BtnSample styles={{ position: 'relative', left: 500, top: 500, width: 120, height: 90, background: 'yellow'}} 
-    onClick={increase} text={counter}/>
-      
-    
+      <BtnSample
+        styles={{
+          position: 'relative',
+          left: 500,
+          top: 500,
+          width: 120,
+          height: 90,
+          background: 'blue',
+        }}
+        onClick={increase}
+        text={counter}
+      />
     </>
   );
 }
